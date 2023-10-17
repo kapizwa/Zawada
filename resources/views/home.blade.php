1caa70e14fe00a9f8bcaca2c90c835ee04c08ca4
kapizwa <kac.gorka@gmail.com>
2023-10-17 11:25:08 +0200

    Dodanie widoku strony glownej

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,81 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="pl">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Stronka</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;400;700&display=swap" rel="stylesheet" />
+    <style>
+        <?php include('../resources/css/home.css'); ?>
+    </style>
+    <script src="https://kit.fontawesome.com/e8dac3ad42.js" crossorigin="anonymous"></script>
+</head>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('Jesteś zalogowany!') }}
-                </div>
-            </div>
+<body>
+    <header>
+        <div class="header-img">
+            <div class="header-bg"></div>
         </div>
-    </div>
-</div>
-@endsection
+    </header>
+    <nav class="nav">
+        <ul>
+            <li class="nav-item"><a href="index.html">home</a></li>
+            <li class="nav-item"><a href="uslugi.html">uslugi</a></li>
+            <li class="nav-item"><a href="cennik.html">cennik</a></li>
+            <li class="nav-item"><a href="kontakt.html">kontakt</a></li>
+        </ul>
+    </nav>
+    <main>
+			<h2>o nas</h2>
+
+			<div class="about-us-box top">
+				<div class="about-us-bg"></div>
+				<div class="about-us-text">
+					<h3>kochamy kawę</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero,
+						veniam qui. In quis, ratione sint laboriosam quaerat minus quidem
+						doloremque!
+					</p>
+				</div>
+			</div>
+
+			<div class="about-us-box middle">
+				<div class="about-us-bg"></div>
+				<div class="about-us-text">
+					<h3>jesteśmy kreatywni</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero,
+						veniam qui. In quis, ratione sint laboriosam quaerat minus quidem
+						doloremque!
+					</p>
+				</div>
+			</div>
+
+			<div class="about-us-box bottom">
+				<div class="about-us-bg"></div>
+				<div class="about-us-text">
+					<h3>lubimy naszą pracę</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero,
+						veniam qui. In quis, ratione sint laboriosam quaerat minus quidem
+						doloremque!
+					</p>
+				</div>
+			</div>
+		</main>
+    <footer>
+        <p>
+            &copy; 2023 Creative Design
+            <span class="icon-box">
+                <a href="#"><i class="fa-brands fa-square-facebook"></i></a>
+                <a href="#"><i class="fa-brands fa-square-twitter"></i></a>
+            </span>
+        </p>
+    </footer>
+</body>
+
+</html>
